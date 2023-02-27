62e7729c8a308f93871be5caf5e0b4189740ab3a
rajupalumaru <raju.palumaru94@gmail.com>
2023-02-27 10:35:02 +0530

    applyed redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useSelector } from 'react-redux';
 import './App.css';
 import Header from './components/Header/Header';
 import Feed from './components/Layouts/Main/Feed';
 import Sidebar from './components/Layouts/Sidebar/Sidebar';
 import Widget from './components/Layouts/Widget/Widget';
 import Login from './components/Login/Login';
+import { selectUser } from './features/userSlice';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  console.log(user);
   return (
     <>
       {
