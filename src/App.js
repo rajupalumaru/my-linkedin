30d069dd7cf39bec74ec9c0a254e04e3f85fc22d
rajupalumaru <raju.palumaru94@gmail.com>
2023-02-27 11:46:17 +0530

    added loin logout functionality in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import { useSelector } from 'react-redux';
+
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import Header from './components/Header/Header';
 import Feed from './components/Layouts/Main/Feed';
 import Sidebar from './components/Layouts/Sidebar/Sidebar';
 import Widget from './components/Layouts/Widget/Widget';
 import Login from './components/Login/Login';
-import { selectUser } from './features/userSlice';
+import { loginuser, logoutuser, selectUser } from './features/userSlice';
+import { auth } from './firebase';
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch=useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(loginuser({
+          email: userAuth.user.email,
+          uid: userAuth.user.uid,
+          photoURL: userAuth.photoURL,
+          displayName: userAuth.displayName,
+      }))
+      }
+      else { 
+        dispatch(logoutuser());
+
+      }
+    })
+  }, [])
   console.log(user);
   return (
     <>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './Login.css';
 import LinkedinLogo from './../../Assets/Linkedin-Logo.png'
+import { auth } from '../../firebase';
+import { useDispatch } from 'react-redux';
+import { loginuser } from '../../features/userSlice';
 
 const Login = () => {
     const [singup, setSignup] = useState(false);
@@ -10,47 +13,84 @@ const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const dispatch = useDispatch();
+
     const register = (e) => {
         e.preventDefault();
-        
-        if(!name){
+
+        if (!name) {
             return alert("Name is required.")
         }
-        if(!PhotoUrl){
+        if (!PhotoUrl) {
             return alert("PhotoUrl is required.")
         }
-        if(!email){
+        if (!email) {
             return alert("email is required.")
         }
-        if(!password){
+        if (!password) {
             return alert("password is required.")
         }
+
+        auth.createUserWithEmailAndPassword(email, password).then((userAuth) => {
+            userAuth.user.updateProfile({
+                displayName: name,
+                photoURL: PhotoUrl,
+            }).then(() => {
+                dispatch(loginuser({
+                    email: userAuth.user.email,
+                    uid: userAuth.user.uid,
+                    photoURL: PhotoUrl,
+                    displayName: name,
+                }))
+            })
+        }).catch(error => alert(error));
+
         setName("");
         setPhotUrl("");
         setEmail("");
         setPassword("");
+    }
 
+    const signIn = (e) => {
+        e.preventDefault();
+        if (!email) {
+            return alert("email is required.")
+        }
+        if (!password) {
+            return alert("password is required.")
+        }
 
+        auth.signInWithEmailAndPassword(email, password).then(({ user }) => {
+            dispatch(loginuser({
+                email: user.email,
+                uid: user.uid,
+                photoURL: user.photoURL,
+                displayName: user.displayName,
+            }))
+        }).then(error => alert(error))
 
     }
+
+
+
     return (
         <div className="loginScreen">
             <img src={LinkedinLogo} />
             {singup === true ? (
 
                 <form onSubmit={register}>
-                    <input type="text" placeholder="Full Name (Required if registered)" value={name} onChange={e=>setName(e.target.value)} />
-                    <input type="text" placeholder="Profile picture Url" value={PhotoUrl} onChange={e=>setPhotUrl(e.target.value)} />
-                    <input type="email" placeholder="Email" value={email} onChange={e=>setEmail(e.target.value)} />
-                    <input type="password" placeholder="Password" value={password} onChange={e=>setPassword(e.target.value)} />
+                    <input type="text" placeholder="Full Name (Required if registered)" value={name} onChange={e => setName(e.target.value)} />
+                    <input type="text" placeholder="Profile picture Url" value={PhotoUrl} onChange={e => setPhotUrl(e.target.value)} />
+                    <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
                     <input type="submit" value='Sign Up' />
 
                     <h4>Already a member ? <span onClick={e => setSignup(false)}>Login Here</span></h4>
                 </form>
             ) : (
-                <form>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                <form onSubmit={signIn}>
+                    <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
                     <input type="submit" value='Sign In' />
 
                     <h4>Not a member ? <span onClick={e => setSignup(true)}>Register Here</span></h4>
